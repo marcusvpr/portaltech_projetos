7a050b7d9955ccbd05dd0be0067c096466ae02d5
MarcusVPR <marcus_vpr@hotmail.com>
2023-10-19 20:29:23 -0300

    feat: Ajuste Function JS

--- portaltech_22/script.js
+++ portaltech_22/script.js
@@ -1,3 +1,7 @@
-console.log('Hello, world!');
-console.log('Hello, world! 1');
-console.log('Hello, world! 2');
+sendMsg('Hello, world! 1');
+sendMsg('Hello, world! 2');
+sendMsg('Hello, world! 3');
+//
+function sendMsg(msg) {
+    console.log('Mensagem: ' + msg);
+}
